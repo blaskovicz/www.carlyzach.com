21c0a49196f87676fa584ea3255d46b3afc1596c
Zach Auclair <zach101@gmail.com>
2018-03-17 11:46:13 -0400

    feat(app): add basic scaffold with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from "react-router-dom";
+//import logo from "./logo.svg";
+import "./App.css";
+
+// routes
+import Home from "./Home";
+import Functions from "./Functions";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <body>
+        <Router>
+          <div>
+            <header>
+              <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <Link to="/" className="navbar-brand">
+                  CarlyZach
+                </Link>
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon" />
+                </button>
+                <div
+                  className="collapse navbar-collapse"
+                  id="navbarSupportedContent"
+                >
+                  <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">
+                        Home
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/functions">
+                        Functions
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+            </header>
+            <main className="App container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/functions" component={Functions} />
+                <Redirect to="/" />
+              </Switch>
+            </main>
+            <footer className="pl-2 footer">
+              <a href="https://github.com/blaskovicz">CarlyZach on Github</a>
+            </footer>
+          </div>
+        </Router>
+      </body>
     );
   }
 }
---
+++ src/Functions/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default () => {
+  return <h1>Functions</h1>;
+};
