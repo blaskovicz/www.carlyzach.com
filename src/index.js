ed2681049e2851a7fc8222a0a63375f1a0bc31b1
Zach Auclair <zach101@gmail.com>
2018-03-18 10:55:03 -0400

    fix(build): es5 popper, page title, favicon

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "jquery/dist/jquery.slim.js";
-import "popper.js/dist/popper.js";
+import "popper.js/dist/umd/popper.js";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "font-awesome/css/font-awesome.css";
