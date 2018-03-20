51cefdcbff3cca1fbd6f897345b4b1fff8df250d
Zach Auclair <zach101@gmail.com>
2018-03-20 01:26:49 -0400

    feat(home): grayscale cats

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,4 +1,23 @@
 import React from "react";
 export default function() {
-  return <h1>Home</h1>;
+  return (
+    <div
+      style={{
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "100% 100%",
+        backgroundImage: `url("https://www.carlyzach.com/cats.jpg")`,
+        maxHeight: "100%",
+        width: "610px",
+        height: "814px",
+        margin: "auto",
+        padding: "0",
+        borderRadius: "10px",
+        border: "1px solid  #ded8d8",
+        "-webkit-filter": "grayscale(100%)",
+        filter: "grayscale(100%)"
+      }}
+    >
+      <p className="pl-2">Hello there.</p>
+    </div>
+  );
 }
