bdcc37e9dd389572030deb7b6342026e7aaddb61
Zach Auclair <zach101@gmail.com>
2018-03-21 20:33:44 -0400

    feat(home): make pic bootstrappy

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,23 +1,21 @@
 import React from "react";
 export default function() {
   return (
-    <div
-      style={{
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "100% 100%",
-        backgroundImage: `url("https://www.carlyzach.com/cats.jpg")`,
-        maxHeight: "100%",
-        width: "610px",
-        height: "814px",
-        margin: "auto",
-        padding: "0",
-        borderRadius: "10px",
-        border: "1px solid  #ded8d8",
-        "-webkit-filter": "grayscale(100%)",
-        filter: "grayscale(100%)"
-      }}
-    >
-      <p className="pl-2">Hello there.</p>
+    <div className="text-center">
+      <img
+        src="/cats.jpg"
+        onError={e => {
+          e.target.src = "//www.carlyzach.com/cats.jpg";
+        }}
+        className="img-fluid rounded"
+        alt="cat pic"
+        style={{
+          maxHeight: "100%",
+          border: "1px solid #ded8d8",
+          WebkitFilter: "grayscale(100%)",
+          filter: "grayscale(100%)"
+        }}
+      />
     </div>
   );
 }
